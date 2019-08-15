7878162dac34f85bcdf42e9ee90f7b59a506ded3
Nikolas Lada <nikolas.lada@gmail.com>
2019-08-15 08:04:01 +0200

    Edited Order\RepositoryTest and changed order table data

--- tests/Order/RepositoryTest.php
+++ tests/Order/RepositoryTest.php
@@ -25,11 +25,11 @@ class RepositoryTest extends \Tests\Base\TestCase
         $connection = $this->container->getByType('\Dibi\Connection');
         $connection->loadFile(__DIR__ . '/sql/table.sql');
         $count = $connection->loadFile(__DIR__ . '/sql/data.sql');
-        $this->assertSame(13, $count);
+        $this->assertSame(5, $count);
     }
 
-    /** @dataProvider dataOrderValue */
     /** @depends testInit */
+    /** @dataProvider dataOrderValue */
     public function testOrder($id, $expectedOrder): void
     {
         $node = $this->tree->getNode($id);
@@ -47,31 +47,31 @@ class RepositoryTest extends \Tests\Base\TestCase
         ];
     }
 
-    /** @dataProvider dataDepthValue */
     /** @depends testOrder */
+    /** @dataProvider dataDepthValue */
     public function testDepth($id, $expectedDepth): void
     {
-        $depth = $this->tree->getDepth($id);
-        $this->assertSame($expectedDepth, $depth);
+        $node = $this->tree->getNode($id);
+        $this->assertSame($expectedDepth, $node->getDepth());
     }
 
     public function dataDepthValue(): array
     {
         return [
-            [1, 1,],
-            [2, 2],
-            [6, 3,],
-            [11, 4,],
-            [13, 4],
+            [1, 0,],
+            [2, 1],
+            [6, 2,],
+            [11, 3,],
+            [13, 3],
         ];
     }
 
-    /** @dataProvider dataParentValue */
     /** @depends testDepth */
+    /** @dataProvider dataParentValue */
     public function testParent($id, $expectedParent): void
     {
-        $parent = $this->tree->getParent($id);
-        $this->assertSame($expectedParent, $parent);
+        $node = $this->tree->getNode($id);
+        $this->assertSame($expectedParent, $node->getParent());
     }
 
     public function dataParentValue(): array
@@ -85,8 +85,8 @@ class RepositoryTest extends \Tests\Base\TestCase
         ];
     }
 
-    /** @dataProvider dataNumberChildsValue */
     /** @depends testParent */
+    /** @dataProvider dataNumberChildsValue */
     public function testNumberChilds($id, $expectedCount): void
     {
         $count = $this->tree->getNumberOfChilds($id);
@@ -110,13 +110,13 @@ class RepositoryTest extends \Tests\Base\TestCase
         $root = $this->tree->getRoot();
         $this->assertSame(1, $root->getId());
 
-        $nodeLíst = $this->tree->getPath($root, false);
-        $this->assertSame(0, $nodeLíst->count());
+        $path = $this->tree->getPath($root, false);
+        $this->assertSame(1, $path->count());
         
-        $nodeList = $this->tree->getBranch($root, \null);
-        $this->assertSame(13, $nodeList->count());
+        $branch = $this->tree->getBranch($root, \null);
+        $this->assertSame(13, $branch->count());
 
-        $this->expectException(\Sakura\Exceptions\InvalidArgumentException::class);
+        $this->expectException(\Sakura\Exceptions\BadArgumentException::class);
         $this->tree->removeNode($root);
     }
 
@@ -139,71 +139,180 @@ class RepositoryTest extends \Tests\Base\TestCase
     /** @depends testPath */
     public function testCreateNodeAfter(): void
     {
-        $previousNode = $this->tree->getNode(5);
+        $nodeA1 = $this->tree->getNode(5);
         $data = [
             'name' => 'BranchE',
         ];
-        $this->tree->createNodeAfter($data, $previousNode);
+        $newId = $this->tree->createNodeAfter($data, $nodeA1);
+        $this->assertSame(14, $newId);
+
+        $nodeA2 = $this->tree->getNode(14);
+        $this->assertSame(14, $nodeA2->getOrder());
+        $this->assertSame(1, $nodeA2->getDepth());
+        $this->assertSame(1, $nodeA2->getParent());
 
         $root = $this->tree->getRoot();
-        $this->expectException(\Sakura\Exceptions\InvalidArgumentException::class);
+        $this->expectException(\Sakura\Exceptions\BadArgumentException::class);
         $this->tree->createNodeAfter($data, $root);
     }
 
     /** @depends testCreateNodeAfter */
     public function testCreateNodeAsFirstChild(): void
     {
-        $previousNode = $this->tree->getRoot();
+        $nodeA1 = $this->tree->getRoot();
         $data = [
             'name' => 'Branch0',
         ];
-        $this->tree->createNodeAfter($data, $previousNode);
+        $newId = $this->tree->createNodeAsFirstChild($data, $nodeA1);
+        $this->assertSame(15, $newId);
+
+        $nodeA2 = $this->tree->getNode(15);
+        $this->assertSame(2, $nodeA2->getOrder());
+        $this->assertSame(1, $nodeA2->getDepth());
+        $this->assertSame(1, $nodeA2->getParent());
     }
 
     /** @depends testCreateNodeAsFirstChild */
     public function testRemoveNode(): void
     {
-        $node = $this->tree->getNode(3);
-        $this->tree->removeNode($node);
+        $nodeA1 = $this->tree->getNode(3);
+        $this->tree->removeNode($nodeA1);
+        $nodeA2 = $this->tree->getNode(3);
+        $this->assertSame(\null, $nodeA2);
+
+        $nodeA2 = $this->tree->getNode(6);
+        $this->assertSame(4, $nodeA2->getOrder());
+        $this->assertSame(1, $nodeA2->getDepth());
+        $this->assertSame(1, $nodeA2->getParent());
     }
 
     /** @depends testRemoveNode */
     public function testMoveBranchAsFirstChild(): void
     {
-        $nodeA = $this->tree->getNode(12);
+        $nodeA1 = $this->tree->getNode(12);
         $goalA = $this->tree->getNode(4);
-        $this->tree->moveBranchAsFirstChild($nodeA, $goalA);
+        $this->tree->moveBranchAsFirstChild($nodeA1, $goalA);
 
-        $nodeB = $this->tree->getNode(4);
+        $nodeA2 = $this->tree->getNode(12);
+        $this->assertSame(7, $nodeA2->getOrder());
+        $this->assertSame(2, $nodeA2->getDepth());
+        $this->assertSame(4, $nodeA2->getParent());
+
+        $nodeB1 = $this->tree->getNode(4);
         $goalB = $this->tree->getNode(2);
-        $this->tree->moveBranchAsFirstChild($nodeB, $goalB);
+        $this->tree->moveBranchAsFirstChild($nodeB1, $goalB);
+
+        $nodeB2 = $this->tree->getNode(4);
+        $this->assertSame(4, $nodeB2->getOrder());
+        $this->assertSame(2, $nodeB2->getDepth());
+        $this->assertSame(2, $nodeB2->getParent());
+
+        $this->expectException(\Sakura\Exceptions\BadArgumentException::class);
+        $nodeC1 = $this->tree->getNode(9);
+        $goalC = $this->tree->getNode(13);
+        $this->tree->moveBranchAfter($nodeC1, $goalC);
     }
 
     /** @depends testMoveBranchAsFirstChild */
+    public function testRootMoveBranchAsFirstChild(): void
+    {
+        $this->expectException(\Sakura\Exceptions\BadArgumentException::class);
+        $nodeA = $this->tree->getNode(1);
+        $goalA = $this->tree->getNode(2);
+        $this->tree->moveBranchAfter($nodeA, $goalA);
+    }
+
+    /** @depends testRootMoveBranchAsFirstChild */
     public function testMoveBranchAfter(): void
     {
-        $nodeA = $this->tree->getNode(8);
-        $goalA = $this->tree->getNode(11);
-        $this->tree->moveBranchAfter($nodeA, $goalA);
+        $nodeA1 = $this->tree->getNode(8);
+        $goalA = $this->tree->getNode(12);
+        $this->tree->moveBranchAfter($nodeA1, $goalA);
 
-        $nodeB = $this->tree->getNode(5);
-        $goalB = $this->tree->getNode(8);
-        $this->tree->moveBranchAfter($nodeB, $goalB);
+        $nodeA2 = $this->tree->getNode(8);
+        $this->assertSame(6, $nodeA2->getOrder());
+        $this->assertSame(3, $nodeA2->getDepth());
+        $this->assertSame(4, $nodeA2->getParent());
+
+        $nodeB1 = $this->tree->getNode(9);
+        $goalB = $this->tree->getNode(4);
+        $this->tree->moveBranchAfter($nodeB1, $goalB);
+
+        $nodeB2 = $this->tree->getNode(9);
+        $this->assertSame(9, $nodeB2->getOrder());
+        $this->assertSame(2, $nodeB2->getDepth());
+        $this->assertSame(2, $nodeB2->getParent());
+
+        $nodeC1 = $this->tree->getNode(4);
+        $goalC = $this->tree->getNode(7);
+        $this->tree->moveBranchAfter($nodeC1, $goalC);
+
+        $nodeC2 = $this->tree->getNode(4);
+        $this->assertSame(8, $nodeC2->getOrder());
+        $this->assertSame(2, $nodeC2->getDepth());
+        $this->assertSame(6, $nodeC2->getParent());
+
+        $this->expectException(\Sakura\Exceptions\BadArgumentException::class);
+        $nodeD1 = $this->tree->getNode(2);
+        $goalD = $this->tree->getNode(13);
+        $this->tree->moveBranchAfter($nodeD1, $goalD);
     }
 
     /** @depends testMoveBranchAfter */
+    public function testRootMoveBranchAfter(): void
+    {
+        $this->expectException(\Sakura\Exceptions\BadArgumentException::class);
+        $nodeA = $this->tree->getNode(2);
+        $goalA = $this->tree->getNode(1);
+        $this->tree->moveBranchAfter($nodeA, $goalA);
+    }
+
+    /** @depends testRootMoveBranchAfter */
     public function testBranch(): void
     {
-        $nodeList = $this->tree->getBranch(1);
+        $root = $this->tree->getRoot();
+        $nodeList = $this->tree->getBranch($root);
         $this->assertSame(14, $nodeList->count());
-
         $this->assertSame(1, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(15, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(2, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(9, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(13, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(6, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(7, $nodeList->current()->getId());
 
         $nodeList->next();
         $this->assertSame(4, $nodeList->current()->getId());
 
         $nodeList->next();
+        $this->assertSame(12, $nodeList->current()->getId());
+
+        $nodeList->next();
         $this->assertSame(8, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(10, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(11, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(5, $nodeList->current()->getId());
+
+        $nodeList->next();
+        $this->assertSame(14, $nodeList->current()->getId());
     }
 
 }
--- tests/Order/sql/data.sql
+++ tests/Order/sql/data.sql
@@ -4,16 +4,16 @@ SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 INSERT INTO `order` (`id`, `order`, `depth`, `parent`, `name`) VALUES
-(1,	1,	1,	NULL,	'Root'),
-(2,	2,	2,	1,	'BranchA'),
-(3,	3,	2,	1,	'BranchB'),
-(4,	6,	2,	1,	'BranchC'),
-(5,	13,	2,	1,	'BranchD'),
-(6,	4,	3,	3,	'BranchBA'),
-(7,	5,	4,	6,	'BranchBAA'),
-(8,	7,	3,	4,	'BranchCA'),
-(9,	10,	3,	4,	'BranchCB'),
-(10,	8,	4,	8,	'BranchCAA'),
-(11,	9,	4,	8,	'BranchCAB'),
-(12,	11,	4,	9,	'BranchCBA'),
-(13,	12,	4,	9,	'BranchCBB');
+(1,	1,	0,	NULL,	'Root'),
+(2,	2,	1,	1,	'BranchA'),
+(3,	3,	1,	1,	'BranchB'),
+(4,	6,	1,	1,	'BranchC'),
+(5,	13,	1,	1,	'BranchD'),
+(6,	4,	2,	3,	'BranchBA'),
+(7,	5,	3,	6,	'BranchBAA'),
+(8,	7,	2,	4,	'BranchCA'),
+(9,	10,	2,	4,	'BranchCB'),
+(10,	8,	3,	8,	'BranchCAA'),
+(11,	9,	3,	8,	'BranchCAB'),
+(12,	11,	3,	9,	'BranchCBA'),
+(13,	12,	3,	9,	'BranchCBB');
